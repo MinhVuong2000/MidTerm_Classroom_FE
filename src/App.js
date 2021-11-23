20e60c69d2d1ef5c6d2ac2271f85a3e7e3c04c53
Vuong Nguyen Thi Minh <minhvuong160620@gmail.com>
2021-11-23 20:46:12 +0700

    Fix bug cannot handle Router

--- src/App.js
+++ src/App.js
@@ -1,34 +1,14 @@
-import ReactDOM from 'react-dom';
-import Classes from './Components/Classes/Classes';
-import FormDialog from './Components/FormDialog/FormDialog';
 import Header from './Components/Header/Header';
-import ClassDetail from './Components/ClassDetail/ClassDetail';
-//import { BrowserRouter as Router, Route } from 'react-router-dom'
-import Detail from './Components/Detail/Detail';
-import React, { Component } from 'react'
-import {
-  BrowserRouter,
-  Routes,
-  Route
-} from "react-router-dom";
+import FormDialog from './Components/FormDialog/FormDialog';
+import React from 'react'
 import RouterURL from './Components/RouterURL/RouterURL';
 
 export default function App() {
     return (
-            <div>
-              <Header/>
-              {/* <FormDialog sx={{display:"flex", justifyContent:"flex-end", marginTop:10}}/> */}
-              <ClassDetail/>
-            </div>
-          )
+      <div>
+        <Header/>
+        {/* <FormDialog sx={{display:"flex", justifyContent:"flex-end", marginTop:10}}/> */}
+        <RouterURL/>
+      </div>
+    )
 }
-
-
-// export default function App() {
-//   return (
-
-//     <div>
-//       <RouterURL></RouterURL>
-//     </div>
-//   )
-// }
--- src/Components/RouterURL/RouterURL.js
+++ src/Components/RouterURL/RouterURL.js
@@ -1,6 +1,8 @@
 import ReactDOM from 'react-dom';
 import Classes from '../Classes/Classes';
 import Detail from '../Detail/Detail';
+import ClassDetail from '../ClassDetail/ClassDetail';
+import NotFound from '../NotFound/NotFound';
 import React, {Component} from 'react'
 import {
   BrowserRouter,
@@ -13,13 +15,37 @@ import {
 
 export default function RouterURL() {
   return (
-
       <BrowserRouter>
         <Routes>
-        <Route path='/' component={Classes} />
-        <Route path='/detail' component={Detail} />
-        <Route component={Error} />
+          <Route exact path='/' element={<Classes/>} />
+          <Route path='/detail' element={<Detail/>} />
+          <Route path='/notfound' component={<NotFound/>} />
         </Routes>
       </BrowserRouter>
   )
 }
+
+// const routes = [
+//     {
+//         path : '/',
+//         exact : true,
+//         main : () => <Classes />
+//     },
+//     {
+//         path : '/detail',
+//         exact : false,
+//         main : () => <Detail />
+//     },
+//     {
+//         path : '/notfound',
+//         exact : false,
+//         main : () => <NotFound />
+//     },
+//     // {
+//     //     path : '/products',
+//     //     exact : false,
+//     //     main : ({ match, location }) => <Products match={match} location={location} />
+//     // }
+// ];
+
+// export default routes;
