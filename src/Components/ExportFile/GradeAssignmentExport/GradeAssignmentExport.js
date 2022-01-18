d70e80eea71cc98fc7b8b760e64932631c317378
trinh172 <tuyettrinhaquarius172@gmail.com>
2022-01-18 08:48:01 +0700

    fix bug assignment export

--- src/Components/ExportFile/GradeAssignmentExport/GradeAssignmentExport.js
+++ src/Components/ExportFile/GradeAssignmentExport/GradeAssignmentExport.js
@@ -4,21 +4,26 @@ import { CSVLink } from 'react-csv'
 import ExitToAppIcon from '@mui/icons-material/ExitToApp';
 
 export const ExportReactCSV = ({csvData, fileName}) => {
+    console.log('Export CSV: ', csvData)
     const listStudent = csvData.listStudentGrade;
     const listAssignment = csvData.listAssignment;
     let custs = []
     for (let i = 0; i < listStudent.length; i++) {
+        console.log("list Student ")
         let student = listStudent[i];
         let temp = {};
         temp['Tên học sinh'] = student.username;
         for(let j = 0; j < listAssignment.length; j++){
             let headerName = listAssignment[j].name + ' (' + listAssignment[j].point + ' điểm)';
-            if(student.assignmentGrade[j].gradeAssignment == null){
-                temp[headerName] = ''
+            if(student.assignmentGrade[j]){
+                if(student.assignmentGrade[j].gradeAssignment == undefined || student.assignmentGrade[j].gradeAssignment == null){
+                    temp[headerName] = ''
+                }
+                else{
+                    temp[headerName] = student.assignmentGrade[j].gradeAssignment.toString();
+                }
             }
-            else{
-                temp[headerName] = student.assignmentGrade[j].gradeAssignment.toString();
-            }
+           
             
         }
         custs.push(temp);
